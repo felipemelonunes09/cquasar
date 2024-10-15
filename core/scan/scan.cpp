1c18b4b6864ea8c6574666d07a1c880e1f0a3963
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-15 00:52:09 -0300

    updated and improved state machine to identify all avaiables tokens and ignore comments

--- core/scan/scan.cpp
+++ core/scan/scan.cpp
@@ -4,7 +4,7 @@
 
 #define BUFFER_LENGHT 256
 
-typedef enum { _START=1, _NUMBER=2, _DONE=3, _ID=4, _EQUAL=5 } ScanState;
+typedef enum { _START=1, _NUMBER=2, _DONE=3, _ID=4, _EQUAL=5, _LESS=6, _GREATER=7, _COMMENT=8 } ScanState;
 
 static char lineBuffer[BUFFER_LENGHT];
 static int linePosition = 0;
@@ -55,11 +55,21 @@ Token* getToken(void) {
                     state = _EQUAL;
                 else if (isWhiteSpace(c))
                     save = false;
+                else if (c == '>')
+                    state = _GREATER;
+                else if (c == '#') {
+                    state = _COMMENT;
+                    save = false;
+                }
                 else {
                     state = _DONE;
                     switch (c)
                     {
-                        case EOF: token->setType(Tt_Ctl_ENDFILE); break;
+                        case EOF: token->setType(Tt_Ctl_ENDFILE);             break;
+                        case '+': token->setType(Tt_Op_Aritc_PLUS);           break;
+                        case '-': token->setType(Tt_Op_Aritc_MINUS);          break;
+                        case '/': token->setType(Tt_Op_Aritc_DIVISION);       break;
+                        case '*': token->setType(Tt_Op_Aritc_MULTIPLICATION); break;
                     }
                 }
             break;
@@ -81,18 +91,42 @@ Token* getToken(void) {
             break;
             case _EQUAL: 
                 if (isEqual(c)) {
-                    token->setType(Tt_Op_Comparation);
+                    token->setType(Tt_Op_Bool_COMPARATION);
                 }
                 else {
-                    token->setType(Tt_Op_Attribuition);
+                    token->setType(Tt_Op_Expr_ATTRIBUITION);
                     ungetNextChat();
                     save = false;
                 }
-                state = _DONE;
+                state=_DONE;
+            break;
+            case _LESS:
+                if (c == '=') 
+                    token->setType(Tt_Op_Bool_LESS_EQUAL);
+                else {
+                    token->setType(Tt_Op_Bool_LESS);
+                    ungetNextChat();
+                    save=false;
+                }
+                state=_DONE;
+            break;
+            case _GREATER:
+                if (c == '=')
+                    token->setType(Tt_Op_Bool_GREATER_EQUAL);
+                else {
+                    token->setType(Tt_Op_Bool_GREATER);
+                    ungetNextChat();
+                    save=false;
+                }
+                state=_DONE;
+            break;
+            case _COMMENT:
+                save=false;
+                if (c == '#')
+                    state=_START;
             break;
             case _DONE: break;
         }
-
         if (save) 
             tokenString += c;
         if (state == _DONE) {
