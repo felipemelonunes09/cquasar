e2651b5c16e17bf5302df10f752d4fcb4f0a56b9
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-10-17 21:10:25 -0300

    added new token else

--- globals.h
+++ globals.h
@@ -18,7 +18,7 @@ extern int linemo; /* enumeration of the lines  of the file source listing */
 typedef enum {
     Tt_Ctl_ENDFILE=0, Tt_Ctl_ERROR=-1, Tt_Ctl_UNDEFINED=-2, Tt_Crl_ENDCOMMAND=-3,
     Tt_Id_NUMBER=1, Tt_Id_IDENTIFIER=2, Tt_Id_O_PARENTHESES=17, Tt_Id_C_PARENTHESES=18,
-    Tt_Rw_IF=3, Tt_Rw_FOR=4, Tt_Rw_WHILE=5, Tt_Rw_PRINT=6,
+    Tt_Rw_IF=3, Tt_Rw_FOR=4, Tt_Rw_WHILE=5, Tt_Rw_PRINT=6, Tt_Rw_Else=19,
         Tt_Op_Expr_ATTRIBUITION=7,
         Tt_Op_Aritc_PLUS=8, Tt_Op_Aritc_MINUS=9, Tt_Op_Aritc_MULTIPLICATION=10, Tt_Op_Aritc_DIVISION=11,
         Tt_Op_Bool_COMPARATION=12, Tt_Op_Bool_GREATER_EQUAL=13, Tt_Op_Bool_GREATER=14, Tt_Op_Bool_LESS=15, Tt_Op_Bool_LESS_EQUAL=16
@@ -35,4 +35,4 @@ extern bool TraceAnalyze;
 extern bool TraceCode;
 extern bool Error;
 
-#endif
+#endiffsdfds
